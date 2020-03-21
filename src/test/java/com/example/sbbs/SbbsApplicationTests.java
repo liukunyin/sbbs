33b3950b288324d5745b60fee88eec9727f9d47b
liukunyin <2500693004@qq.com>
2020-03-21 11:54:31 +0800

    first

---
+++ src/test/java/com/example/sbbs/SbbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
